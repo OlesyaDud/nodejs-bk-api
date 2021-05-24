6dc5934404ef583055bb42c0b53a0dc11c38e161
OlesyaDud <wodadevelopment@gmail.com>
2021-05-24 11:49:25 -0400

    morgan logger added, controllers created

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,25 +1,23 @@
 const express = require('express');
+const {
+    createBootcamp, 
+    updateBootcamp, 
+    deleteBootcamp, 
+    getBootcamps, 
+    getBootcamp
+} = require('../controllers/bootcamps');
+
 const router = express.Router();
 
-// many
-router.get('/', (req, res) => {
-    res.status(200).json({success: true, msg: 'Show all bootcamps'});
-});
-// single
-router.get('/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Get bootcamp ${req.params.id}`});
-});
+router.route('/')
+.get(getBootcamps)
+.post(createBootcamp);
 
-router.post('/', (req, res) => {
-    res.status(200).json({success: true, msg: 'Create new bootcamp'});
-});
+router.route('/:id')
+.get(getBootcamp)
+.put(updateBootcamp)
+.delete(deleteBootcamp);
 
-router.put('/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`});
-});
 
-router.delete('/:id', (req, res) => {
-    res.status(200).json({success: true, msg: `Delete bootcamp ${req.params.id}`});
-});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 
 // Load evn variables
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// custom middleware
+// app.use(logger);
+
+// Dev logging middleware
+if(process.env.NODE_ENV === "development") {
+    app.use(morgan('dev'));
+}
+
 
 // Routes mounting
 app.use('/api/v1/bootcamp/', bootcamps);
