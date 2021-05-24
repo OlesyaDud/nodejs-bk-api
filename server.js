0a02032f40a6bc8ebb5a026021c4f3038fa85eda
OlesyaDud <wodadevelopment@gmail.com>
2021-05-24 12:53:24 -0400

    connection to db established

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const bootcamps = require('./routes/bootcamps');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
-
+const connectDB = require('./config/db');
 
 // Load evn variables
 dotenv.config({path: './config/config.env'});
+
+// connect to db
+connectDB();
+
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -24,4 +28,12 @@ app.use('/api/v1/bootcamp/', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+
+// handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    // close server and exist process
+    server.close(() => process(1));
+});
