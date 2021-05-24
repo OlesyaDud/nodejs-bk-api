bf862a7ea06efd7dc53c60eea4de1f3a424d2311
OlesyaDud <wodadevelopment@gmail.com>
2021-05-24 11:06:54 -0400

    simple routes

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bootcamps = require('./routes/bootcamps');
+
 
 // Load evn variables
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// Routes mounting
+app.use('/api/v1/bootcamp/', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
