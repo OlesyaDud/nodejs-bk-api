af2217c9aa5e722d28fd447c1e77ad6df5a1ffde
OlesyaDud <wodadevelopment@gmail.com>
2021-06-02 14:45:33 -0400

    update user password/email

--- controllers/auth.js
+++ controllers/auth.js
@@ -60,6 +60,41 @@ exports.getLoggedUser = asyncHandler(async(req, res, next) => {
     })
 });
 
+// @desc  update user details(name and email only)
+// @route PUT /api/v1/auth/updatedetails
+// @access  Private
+exports.updateDetails = asyncHandler(async(req, res, next) => {
+    const fieldsToUpdate = {
+        name: req.body.name,
+        email: req.body.email
+    }
+    const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate);
+
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+});
+
+// @desc  update password
+// @route PUT /api/v1/auth/updatepassword
+// @access  Private
+exports.updatePassword = asyncHandler(async(req, res, next) => {
+    const user = await User.findById(req.user.id).select('+password');
+
+    // check current password
+    // await because matchPassword in a model is a Promise
+    if(!(await user.matchPassword(req.body.currentPassword))) {
+        return next(new ErrorResponse('Password is incorrec', 401));
+    }
+
+    user.password = req.body.newPassword;
+
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
+});
+
 // @desc  forgot password
 // @route POST /api/v1/auth/forgotpassword
 // @access  Public
